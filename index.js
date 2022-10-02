92ac8972c84cc2cf4a610d4054e24814df1220a5
Vladyslav <vkadyrov@protonmail.com>
2022-10-02 16:23:02 -0400

    commit tree 63d9ad7236ea88a04bf7bb175c86541a01651182

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const idMain = document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+const text = document.createTextNode('Vlad is the champion');
+
+newHeader.appendChild(text);
